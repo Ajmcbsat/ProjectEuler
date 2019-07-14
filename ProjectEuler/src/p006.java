cf2809b8cf5918bc4d24385e0a932c531537b55d
Kamil Adem <Kamil Adem@192.168.3.55>
2019-07-13 23:29:42 -0400

    Project Euler Problem 6 w/ JavaDocs

--- ProjectEuler/src/p006.java
+++ ProjectEuler/src/p006.java
@@ -2,12 +2,12 @@
  * This class solves problem six in Project Euler.
  * 
  * The sum of the squares of the first ten natural numbers is,
- * 12 + 22 + ... + 102 = 385
+ * 1^2 + 2^2 + ... + 10^2 = 385
  * The square of the sum of the first ten natural numbers is,
- * (1 + 2 + ... + 10)2 = 552 = 3025
+ * (1 + 2 + ... + 10)^2 = 552 = 3025
  * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
  * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-
+ * 
  * This solution uses a brute force algorithm
  */
 
@@ -18,15 +18,38 @@
 public class p006 implements Euler {
 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	/**
+	 * @return the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum
+	 */
 	@Override
 	public String run() {
-		// TODO Auto-generated method stub
-		return null;
+		return Long.toString(squareOfSum()-sumOfSquares());
+	}
+	
+	/**
+	 * This method computes the sum of the square of each number 1-100
+	 * 
+	 * @return the sum
+	 */
+	public long sumOfSquares() {
+		long sum =0;
+		for (int i=1; i<101; i++) {
+			sum+=Math.pow(i, 2);
+		}
+		return sum;
 	}
 
+	/**
+	 * This method computes the square of the sums of values 1-100
+	 * 
+	 * @return the sum
+	 */
+	public long squareOfSum() {
+		long sum = 0;
+		for (int i=1; i<101; i++) {
+			sum+=i;
+		}
+		
+		return (long) Math.pow(sum, 2);
+	}
 }
