c9471a88bd63b412939a86d4467e527c4cc10b0b
Kamil Adem <Kamil Adem@192.168.3.55>
2019-07-13 23:32:38 -0400

    Updated JavaDocs

--- ProjectEuler/src/Euler.java
+++ ProjectEuler/src/Euler.java
@@ -1,5 +1,17 @@
+/**
+ * Interface which requires a String method run()
+ */
 
+/**
+ * @author Kamil Adem
+ *
+ */
 public interface Euler {
 	
+	/**
+	 * Only required method which executes the algorithm for the solution
+	 * 
+	 * @return solution in string format
+	 */
 	String run(); 
 }
