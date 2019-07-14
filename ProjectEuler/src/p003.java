7b3c870cdac01bfa6f2f2238d5a2bee82f3a306d
Kamil Adem <Kamil Adem@192.168.3.55>
2019-07-13 22:40:14 -0400

    Updated JavaDocs

--- ProjectEuler/src/p003.java
+++ ProjectEuler/src/p003.java
@@ -1,4 +1,11 @@
 /**
+ * This class solves problem three in Project Euler.
+ * 
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143?
+ * 
+ * This solution uses a brute force algorithm 
+ * 
  * @author Kamil Adem
  *
  */
--- ProjectEuler/src/p005.java
+++ ProjectEuler/src/p005.java
@@ -1,21 +1,32 @@
-/**
- * 
- */
 
 /**
+ * This class solves problem five present in Project Euler.
+ * 
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ * 
+ * This solution uses the concept of dynamic programming
+ * 
  * @author Kamil Adem
  *
  */
+
 public class p005 implements Euler {
 
-	private int [] val = new int[20];
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println(new p005().run());
-	}
 	
+	/**
+	 * THe array which holds the max counts of each prime factor in the range 1-20
+	 */
+	private int [] val = new int[20];
+
+	/**
+	 * This method computes the smallest multiple that can be divided by all numbers 1-20
+	 * This method computes the prime factors of every number in the range 2-20, stores those 
+	 * values in an array and update the final array based on the max count of each prime number.
+	 * The final product is then computed by multiplying the values in the final array.
+	 * 
+	 *@return the final product as a String
+	 */
 	@Override
 	public String run() {
 		for (int i=2; i<21; i++) {
@@ -28,12 +39,12 @@ public class p005 implements Euler {
 	}
 
 	/**
-	 * This method computes the smallest prime factor of a number.
-	 * If there are no prime factors left in the range, then the
-	 * remaining number is prime
+	 * This method computes the prime factors of a number.
+	 * Recursively computes prime factors of the number and
+	 * updates the given array's values
 	 * 
+	 * @param arr the array that will hold the prime factors
 	 * @param num the integer 
-	 * @return    the smallest prime factor of the number
 	 */
 	public void primeFactor(int [] arr,int num) {
 		for (int i=2; i<=num; i++) {
@@ -46,12 +57,28 @@ public class p005 implements Euler {
 		return;
 	}
 
+	/**
+	 * This method updates the first array based on the second array's values
+	 * The update is based on the max value at each index.
+	 * 
+	 * @param arr1 the first array
+	 * @param arr2 the second array
+	 */
 	public void update(int[] arr1, int[] arr2) {
 		for (int i=0; i<20; i++) {
 			arr1[i] = Integer.max(arr1[i], arr2[i]);
 		}
 	}
 	
+	
+	/**
+	 * Based on the final array, this method computes the final product
+	 * Using the values obtained by computing the prime factors, the values
+	 * present in the array are multiplied by their respective counts
+	 * 
+	 * @param arr the final array containing the counts of each prime number
+	 * @return    the final product
+	 */
 	public int smallestMultiple(int[] arr) {
 		int num = 1;
 		for (int i=0; i<20; i++) {
