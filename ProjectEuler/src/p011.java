4bbde6f5014f6b37db69a9d6bce371e16cd319fb
Kamil Adem <Kamil Adem@192.168.3.55>
2019-07-15 22:00:45 -0400

    Problem 11 Template

---
+++ ProjectEuler/src/p011.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kamil Adem
+ *
+ */
+public class p011 implements Euler {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public String run() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
