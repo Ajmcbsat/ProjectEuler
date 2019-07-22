eb09bd263c349aadd3097d1f71a7e264517b8237
Kamil Adem <Kamil Adem@192.168.3.55>
2019-07-22 12:23:01 -0400

    Problem 24

---
+++ ProjectEuler/src/p024.java
@@ -0,0 +1,31 @@
+/**
+ * This class solve Problem 24 in Project Euler.
+ * 
+ * A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. 
+ * If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. 
+ * The lexicographic permutations of 0, 1 and 2 are:
+ * 
+ * 012   021   102   120   201   210
+ * 
+ * What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+ * 
+ * @author Kamil Adem
+ *
+ */
+public class p024 implements Euler {
+
+	@Override
+	public String run() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
